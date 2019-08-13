18fb815cea7096c2dd3241a47f7c468f6d2bff01
robbedec <robbe.decorte@student.hogent.be>
2019-08-13 19:22:50 +0200

    [style] Change about-me page layout

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -6,14 +6,14 @@ import SEO from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
-    <div>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    
-      <div>fqkfjqsmkfsqjdfmqkfljqsdmfkqjsmfkqsjflqdslmkjfmqfjkqmsjdfkljqlsdjfmkqsfjlqkfjmqksdjqdlqfmdsjfkqjdsflqkjmjqkdjmqjfs</div>
-    
-    <Link to="/">Go back to the homepage</Link>
+    <div className="about">
+      <SEO title="Page two" />
+      <h1 className="h1-wrap">About me</h1>
+      <p>Welcome to page 2</p>
+      
+        <div>fqkfjqsmkfsqjdfmqkfljqsdmfkqjsmfkqsjflqdslmkjfmqfjkqmsjdfkljqlsdjfmkqsfjlqkfjmqksdjqdlqfmdsjfkqjdsflqkjmjqkdjmqjfs</div>
+      
+      <Link to="/">Go back to the homepage</Link>
     </div>
   </Layout>
 )
