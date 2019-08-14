708ecbcb4613efea07ff39e21ff4475030ef8dce
robbedec <robbe.decorte@student.hogent.be>
2019-08-14 20:43:00 +0200

    [fix] Image styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import { Link } from 'gatsby';
+import Img from 'gatsby-image';
 import Avatar from '../../content/assets/images/avatar.jpg';
 
 import Layout from '../components/layout';
@@ -17,7 +18,15 @@ class AboutPage extends React.Component {
           <SEO title="Page two" />
           <h1 className="h1-wrap">About me</h1>
           <div className="about-content">
-            <img src={Avatar} alt="profile" className="avatar" />
+            <Img
+              className="avatar"
+              style={{
+                width: '15vw',
+                height: '30vh',
+                backgroundSize: 'cover',
+              }}
+              fixed={data.avatar.childImageSharp.fixed}
+            />
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet
@@ -47,124 +56,6 @@ class AboutPage extends React.Component {
               dictum. Vulputate eu scelerisque felis imperdiet proin fermentum
               leo vel. Scelerisque varius morbi enim nunc faucibus a
               pellentesque. Lectus magna fringilla urna porttitor rhoncus dolor
-              purus. Cras ornare arcu dui vivamus arcu felis bibendum ut. Amet
-              facilisis magna etiam tempor orci eu. Suspendisse sed nisi lacus
-              sed viverra tellus in hac. Ac ut consequat semper viverra nam.
-              Neque egestas congue quisque egestas diam. Elit duis tristique
-              sollicitudin nibh sit amet commodo. Cras ornare arcu dui vivamus
-              arcu felis bibendum ut tristique. Faucibus a pellentesque sit amet
-              porttitor eget dolor. Morbi tincidunt augue interdum velit euismod
-              in pellentesque massa placerat. In pellentesque massa placerat
-              duis. Venenatis urna cursus eget nunc. Nisi lacus sed viverra
-              tellus in hac habitasse. Donec adipiscing tristique risus nec
-              feugiat in. Euismod nisi porta lorem mollis aliquam ut porttitor
-              leo. Nisl nisi scelerisque eu ultrices vitae auctor. Sit amet
-              volutpat consequat mauris nunc congue nisi. Quam nulla porttitor
-              massa id neque aliquam. Facilisis magna etiam tempor orci eu
-              lobortis elementum nibh. Faucibus turpis in eu mi bibendum neque
-              egestas congue. Faucibus vitae aliquet nec ullamcorper sit amet
-              risus. A pellentesque sit amet porttitor eget dolor. Nunc
-              consequat interdum varius sit. Non pulvinar neque laoreet
-              suspendisse interdum. Eget mauris pharetra et ultrices neque.
-              Malesuada fames ac turpis egestas sed tempus urna et. Tincidunt
-              arcu non sodales neque sodales ut. Nulla facilisi nullam vehicula
-              ipsum a arcu cursus vitae. Sed viverra tellus in hac. Aenean
-              euismod elementum nisi quis. Faucibus scelerisque eleifend donec
-              pretium. Aliquam sem et tortor consequat id. Vulputate eu
-              scelerisque felis imperdiet proin. Quis eleifend quam adipiscing
-              vitae proin sagittis nisl rhoncus mattis. Nulla facilisi etiam
-              dignissim diam. Malesuada nunc vel risus commodo viverra. Hac
-              habitasse platea dictumst quisque sagittis purus sit. Ullamcorper
-              morbi tincidunt ornare massa eget. Ornare arcu dui vivamus arcu
-              felis bibendum ut tristique et. Felis eget nunc lobortis mattis
-              aliquam faucibus purus in. Tempus imperdiet nulla malesuada
-              pellentesque. Augue lacus viverra vitae congue eu consequat ac.
-              Risus ultricies tristique nulla aliquet enim. Porttitor massa id
-              neque aliquam vestibulum morbi blandit cursus. Augue interdum
-              velit euismod in. Tincidunt dui ut ornare lectus sit amet.
-              Scelerisque mauris pellentesque pulvinar pellentesque habitant
-              morbi tristique. Pulvinar elementum integer enim neque volutpat ac
-              tincidunt vitae. Sociis natoque penatibus et magnis dis parturient
-              montes. Congue quisque egestas diam in arcu cursus. Pellentesque
-              nec nam aliquam sem et tortor. Adipiscing bibendum est ultricies
-              integer. Sagittis nisl rhoncus mattis rhoncus. Dictum non
-              consectetur a erat. Odio pellentesque diam volutpat commodo sed
-              egestas. Congue nisi vitae suscipit tellus mauris a. Congue nisi
-              vitae suscipit tellus mauris a diam maecenas. Diam vel quam
-              elementum pulvinar etiam. Nunc aliquet bibendum enim facilisis
-              gravida neque. Sed vulputate odio ut enim blandit volutpat.
-              Malesuada proin libero nunc consequat interdum. Sit amet massa
-              vitae tortor condimentum lacinia quis. Morbi tristique senectus et
-              netus et malesuada fames. Pellentesque habitant morbi tristique
-              senectus. Purus faucibus ornare suspendisse sed nisi. Quam
-              elementum pulvinar etiam non quam lacus suspendisse faucibus
-              interdum. Fringilla phasellus faucibus scelerisque eleifend donec.
-              Pretium aenean pharetra magna ac placerat. Risus commodo viverra
-              maecenas accumsan lacus vel. Viverra vitae congue eu consequat ac
-              felis donec. Nisl suscipit adipiscing bibendum est ultricies
-              integer quis auctor. Commodo viverra maecenas accumsan lacus vel
-              facilisis volutpat. Accumsan sit amet nulla facilisi morbi tempus.
-              Faucibus turpis in eu mi bibendum neque. Nec dui nunc mattis enim
-              ut tellus elementum sagittis. Vivamus at augue eget arcu dictum.
-              Mi quis hendrerit dolor magna eget est lorem ipsum. Tincidunt
-              lobortis feugiat vivamus at augue eget. Tortor id aliquet lectus
-              proin nibh. Vulputate sapien nec sagittis aliquam malesuada
-              bibendum arcu vitae. Sollicitudin aliquam ultrices sagittis orci a
-              scelerisque purus semper eget. Vitae justo eget magna fermentum
-              iaculis. Consequat interdum varius sit amet mattis. Vel risus
-              commodo viverra maecenas accumsan. Auctor augue mauris augue neque
-              gravida. Tellus elementum sagittis vitae et leo duis. Augue eget
-              arcu dictum varius duis. Elit eget gravida cum sociis natoque
-              penatibus et magnis. Sollicitudin nibh sit amet commodo. Quam
-              nulla porttitor massa id neque aliquam. Cursus mattis molestie a
-              iaculis at erat pellentesque. Vestibulum sed arcu non odio. Nibh
-              venenatis cras sed felis eget. Porta non pulvinar neque laoreet
-              suspendisse interdum consectetur libero. Enim sit amet venenatis
-              urna. Vestibulum sed arcu non odio euismod. Nibh tortor id aliquet
-              lectus proin nibh nisl condimentum id. Eget nunc lobortis mattis
-              aliquam faucibus purus in massa. Mattis aliquam faucibus purus in
-              massa tempor nec feugiat nisl. Condimentum lacinia quis vel eros
-              donec ac odio tempor orci. Dolor sit amet consectetur adipiscing.
-              Tempus iaculis urna id volutpat lacus. Massa id neque aliquam
-              vestibulum. Amet consectetur adipiscing elit pellentesque habitant
-              morbi tristique senectus. Nisl purus in mollis nunc sed id semper.
-              Morbi tristique senectus et netus et malesuada fames ac. Sit amet
-              cursus sit amet dictum. Mauris pharetra et ultrices neque ornare
-              aenean euismod elementum. Sagittis id consectetur purus ut
-              faucibus pulvinar. Eget mauris pharetra et ultrices neque ornare.
-              Ut tellus elementum sagittis vitae et leo duis ut diam. Iaculis eu
-              non diam phasellus. Enim blandit volutpat maecenas volutpat. Nulla
-              at volutpat diam ut venenatis tellus in. Mi eget mauris pharetra
-              et ultrices neque ornare. Nec feugiat in fermentum posuere urna
-              nec tincidunt. At varius vel pharetra vel turpis nunc eget. Lacus
-              suspendisse faucibus interdum posuere lorem ipsum. Malesuada nunc
-              vel risus commodo viverra maecenas accumsan lacus vel. Ut aliquam
-              purus sit amet luctus venenatis lectus. Sit amet risus nullam eget
-              felis eget nunc lobortis mattis. Gravida dictum fusce ut placerat.
-              Integer vitae justo eget magna fermentum iaculis. Phasellus
-              vestibulum lorem sed risus ultricies. Volutpat est velit egestas
-              dui id ornare arcu odio ut. Diam quam nulla porttitor massa id
-              neque. Pretium lectus quam id leo in. Dictumst quisque sagittis
-              purus sit amet volutpat consequat mauris nunc. Ultricies mi eget
-              mauris pharetra et ultrices neque. Sed faucibus turpis in eu mi
-              bibendum neque egestas. Lacinia quis vel eros donec. Sit amet
-              volutpat consequat mauris nunc. Aliquet sagittis id consectetur
-              purus ut faucibus pulvinar elementum integer. Vivamus at augue
-              eget arcu dictum varius duis at. Adipiscing elit duis tristique
-              sollicitudin nibh sit amet commodo. In aliquam sem fringilla ut.
-              Et leo duis ut diam quam. Sagittis aliquam malesuada bibendum arcu
-              vitae elementum curabitur vitae. A condimentum vitae sapien
-              pellentesque. Arcu non odio euismod lacinia at quis. Tincidunt dui
-              ut ornare lectus sit amet est placerat. Facilisis volutpat est
-              velit egestas dui id ornare arcu. Consequat ac felis donec et odio
-              pellentesque diam. Duis ultricies lacus sed turpis tincidunt.
-              Etiam sit amet nisl purus in. Sed libero enim sed faucibus.
-              Suscipit adipiscing bibendum est ultricies. Aliquam malesuada
-              bibendum arcu vitae elementum curabitur. Porttitor rhoncus dolor
-              purus non enim praesent elementum. Et odio pellentesque diam
-              volutpat commodo sed egestas. Eget nullam non nisi est sit amet
-              facilisis magna etiam. Vitae nunc sed velit dignissim sodales.
-              Enim facilisis gravida neque convallis a cras semper auctor neque.
             </p>
           </div>
           <div className="link-home">
@@ -184,6 +75,13 @@ export const pageQuery = graphql`
     site {
       siteMetadata {
         title
+      }
+    }
+    avatar: file(absolutePath: { regex: "/images/avatar.jpg/" }) {
+      childImageSharp {
+        fixed(width: 665, height: 672) {
+          ...GatsbyImageSharpFixed
+        }
       }
     }
   }
